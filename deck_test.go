a8f2a08e2e74d3a2c33375e8a8c3d763da84203d
Peter Sowah <petersowah@gmail.com>
2023-12-14 15:23:48 +0000

    wip

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -18,4 +21,21 @@ func TestNewDeck(t *testing.T) {
 	if d[deckLength-1] != "Four of Clubs" {
 		t.Errorf("Expected last card of Four of Clubs, but got %v", d[deckLength-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	os.Remove("_decktesting")
+
+	d := newDeck()
+	d.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	deckLength := len(loadedDeck)
+
+	if deckLength != 16 {
+		t.Errorf("Expected deck length of 16, but got %v", deckLength)
+	}
+
+	os.Remove("_decktesting")
 }
